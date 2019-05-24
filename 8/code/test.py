c80823c578e15948a799e2c3a3083323ae25f398
landmarkcsl <313728420@qq.com>
2019-05-24 11:56:14 +0800

    code

--- 8/code/test.py
+++ 8/code/test.py
@@ -3,13 +3,11 @@
 # name = 'tom'
 # s1 = s1.format(year,name)
 #
-# s1 = " I am {year} old,I am {name}"
+s1 = " I am {:^20} old,I am {:30}"
 #
-# # s1 = s1.format(name='tom',year=20)
-#
-# d = {'name':'tom','year':16}
-# s1 = s1.format(**d)
-# print(s1)
+s1 = s1.format('tom',20)
 
-a = ('3','5')
-print('-'.join(a))
+print(s1)
+#
+# a = ('3','5')
+# print('-'.join(a))
